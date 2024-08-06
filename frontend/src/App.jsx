5fc4a1ea748202b79c044c1c14c5b81db81ab2e5
Ayushman2694 <sahuayushman4@gmail.com>
2024-08-06 13:50:53 +0530

    fixed pacakge.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
-import LearnerDashboard from "./pages/LearnerDashboard";
-import Login from "./pages/Login";
-import AppLayout from "./ui/AppLayout";
-=======
-import AppLayout from "./ui/AppLayout";
-import Login from "./pages/Login";
 
+
+import AppLayout from "./ui/AppLayout";
+import Login from "./pages/Login";
 import EmployeeInfo from "./pages/Employee_Info";
 import Dashboard from "./pages/Dashboard";
 import Settings from "./pages/Settings";
 import Courses from "./pages/Courses";
->>>>>>> upstream/main
+
 export default function App() {
   return (
     <BrowserRouter>
