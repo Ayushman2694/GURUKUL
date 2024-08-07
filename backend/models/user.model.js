1850e39bbea395121cdff2ca54530147c768669a
Satyam Singh <116868686+satyamsingh22@users.noreply.github.com>
2024-08-07 16:58:30 +0530

    protect route

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,29 +1,29 @@
 import mongoose from "mongoose";
 
 const employeeSchema = new mongoose.Schema({
-    employeeName:{
-        type:String,
-        required:true,
+    employeeName: {
+        type: String,
+        required: true,
     },
-    empId:{
-        type:String,
-        required:true,
-        unique:true
+    empId: {
+        type: String,
+        required: true,
+        unique: true
     },
-    designation:{
-        type:String,
-        required:true
+    designation: {
+        type: String,
+        required: true
     },
-    department:{
-        type:String,
-        required:true
+    department: {
+        type: String,
+        required: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     }
-},{timestamp:true});
+}, { timestamps: true });
 
-const Employee = mongoose.model("Employee",employeeSchema);
+const Employee = mongoose.model("Employee", employeeSchema);
 
 export default Employee;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,20 +5,31 @@ import Dashboard from "./pages/Dashboard";
 import EmployeeInfo from "./pages/Employee_Info";
 import Courses from "./pages/Courses";
 import Settings from "./pages/Settings";
+import PrivateRoute from "./component/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<Navigate to="/dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="profile" element={<EmployeeInfo />} />
-          <Route path="courses" element={<Courses />} />
-          <Route path="settings" element={<Settings />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="*"
+        element={
+          <PrivateRoute>
+            <AppLayout>
+              <Routes>
+                <Route path="/" element={<Navigate to="/dashboard" />} />
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="profile" element={<EmployeeInfo />} />
+                <Route path="courses" element={<Courses />} />
+                <Route path="settings" element={<Settings />} />
+              </Routes>
+            </AppLayout>
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+  </BrowserRouter>
+
   );
 }
