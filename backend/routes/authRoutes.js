8426f565a0cb793fa96dfafe8559c29070eca55c
Ayushman2694 <sahuayushman4@gmail.com>
2024-08-09 13:58:49 +0530

    admin login api added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { login, verifyToken } from "../controllers/auth.controllers.js";
 import { changePassword } from "../controllers/changePassword.controller.js";
+import { adminLogin } from "../controllers/admin.auth.controller.js";
 const router = express.Router();
 
 router.post("/login", login);
 router.get('/verifyToken', verifyToken);
 router.post('/changePassword',changePassword);
+router.post("/adminLogin",adminLogin)
 
 export default router;
