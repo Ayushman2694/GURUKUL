e2f6437f6dc7af6f35292ccc2243d694a4200423
Satyam Singh <116868686+satyamsingh22@users.noreply.github.com>
2024-08-10 17:48:40 +0530

    course api

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./db/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import courseRouter from "./routes/courseRoutes.js";
 const app = express();
 const port = 6300;
 dotenv.config();
@@ -14,6 +15,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
+app.use("/thumbnail",express.static('uploads'))
+app.use("/api/course",courseRouter)
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
