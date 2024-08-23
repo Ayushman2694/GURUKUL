fe2691007445828589ee210518e27fb57ab68eee
Satyam Singh <116868686+satyamsingh22@users.noreply.github.com>
2024-08-23 15:24:46 +0530

    Assign course

--- backend/controllers/enrollCourse.controller.js
+++ backend/controllers/enrollCourse.controller.js
@@ -1,38 +1,34 @@
 import Course from "../models/course.model.js";
 import Employee from "../models/user.model.js";
 
-export const assignCourse = async(req,res)=>{
-    try{
-        const {empId , courseId}= req.params;
-        console.log("employeeid ",empId, "Received courseId:", courseId)
+export const assignCourse = async (req, res) => {
+  try {
+    const { empId, courseId } = req.body;
+    console.log("Employee ID:", empId, "Received course ID:", courseId);
 
-        const employee = await Employee.findOne({empId})
-        if(!employee){
-            return res.status(400).json({ message: "Employee not found" });
-
-        }
-
-        const course = await Course.findById(courseId);
-        if (!course) {
-          return res.status(400).json({ message: "Course not found" });
-        }
-
-        if(!employee.courses.includes(courseId)){
-            employee.courses.push(courseId)
-            await employee.save()
-        }
-        else {
-            return res.status(400).json({ message: "Course already assigned to the employee" });
-          }
-
-
-
-    }catch(error){
-        console.log(error)
-
+    const employee = await Employee.findOne({ empId });
+    if (!employee) {
+      return res.status(400).json({ message: "Employee not found" });
     }
 
-}
+    const course = await Course.findById(courseId);
+    if (!course) {
+      return res.status(400).json({ message: "Course not found" });
+    }
+
+    if (!employee.courses.includes(courseId)) {
+      employee.courses.push(courseId);
+      await employee.save();
+      return res.status(200).json({ message: "Course assigned successfully" });
+    } else {
+      return res.status(400).json({ message: "Course already assigned to the employee" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred while assigning the course" });
+  }
+};
+
 
 
 
