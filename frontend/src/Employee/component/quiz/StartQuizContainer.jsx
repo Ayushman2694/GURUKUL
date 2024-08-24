cd829f626955e6cf6b372f0eccc3d6008129fb36
Priyanshu Kushwaha <priyanshukushwaha311@gmail.com>
2024-08-24 11:58:45 +0530

    responsiveness

--- frontend/src/Employee/component/quiz/StartQuizContainer.jsx
+++ frontend/src/Employee/component/quiz/StartQuizContainer.jsx
@@ -4,11 +4,11 @@ export default function StartQuizContainer() {
 
   const navigate = useNavigate();
   return (
-    <div className="w-1/2 p-2">
+    <div className="w-full p-2">
       <div className="shadow-lg shadow-stone-300 border rounded-md p-3">
         <p className="text-lg font-bold py-1">Quiz:1</p>
         <p className="text-md  py-1 ">
-          Test your knowledge on the basis of this module
+          Test your knowledge on the basis of this module check it fast
         </p>
         <button className=" bg-blue-600 text-slate-50 text-md font-bold rounded-lg py-1 px-2 my-2" 
           onClick={() => {
--- frontend/src/Employee/pages/Course.jsx
+++ frontend/src/Employee/pages/Course.jsx
@@ -1,3 +1,64 @@
+// /* eslint-disable no-unused-vars */
+// import { useParams } from "react-router-dom";
+// import Module from "../component/courses/Module";
+// import StartQuizContainer from "../component/quiz/StartQuizContainer";
+// import { useModuleByCourseId } from "../../Admin/components/courses/useModuleByCourseId";
+// import Spinner from "../../Common/Ui/Spinner";
+// import { useState } from "react";
+// import ReactPlayer from "react-player";
+// import VideoPlayer from "./VideoPlayer";
+// import { useCourseByCourseId } from "../../Admin/components/courses/useCourseByCourseId";
+
+// export default function Course() {
+//   const { courseId } = useParams();
+//   const [videoLink, setVideoLink] = useState("");
+//   const [videoDiscription, setVideoDiscription] = useState();
+//   const { isLoading: loadingModule, modules } = useModuleByCourseId(courseId);
+//   const { isLoading: loadingCourse, course } = useCourseByCourseId(courseId);
+
+//   if (loadingModule || loadingCourse) return <Spinner />;
+//   return (
+//     <div className="w-full grid md:flex p-4">
+//       <div className="md:w-9/12 overflow-y-auto pb-20">
+//         <div>
+//             {/* {videoLink && <ReactPlayer url={videoLink} width="100%" controls />} */}
+//           {videoLink ? (
+//             <VideoPlayer videoLink={videoLink} />
+//           ) : (
+//             <img src={course?.thumbnail} className="w-full rounded" />
+//           )}
+//         </div>
+
+//         <div className="w-full shadow-lg shadow-stone-400 rounded-lg border py-4 mt-8">
+//           <p className="text-xl font-bold px-3"> Quizzes</p>
+//           <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-2 mb-8">
+//             <StartQuizContainer />
+//             <StartQuizContainer />
+//             <StartQuizContainer />
+//           </div>
+//         </div>
+//       </div>
+
+//       <div className="md:w-3/12 h-full  px-2 pb-16 ">
+//         <div className="bg-slate-100 h-full shadow-lg shadow-stone-400 rounded-md overflow-y-auto">
+//           {modules.map((module) => (
+//             <Module
+//               key={module._id}
+//               videos={module.video}
+//               moduleName={module.moduleName}
+//               setVideoLink={setVideoLink}
+//               setVideoDiscription={setVideoDiscription}
+//             />
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+
+
+
 /* eslint-disable no-unused-vars */
 import { useParams } from "react-router-dom";
 import Module from "../component/courses/Module";
@@ -18,27 +79,19 @@ export default function Course() {
 
   if (loadingModule || loadingCourse) return <Spinner />;
   return (
-    <div className="w-full flex p-4">
-      <div className="w-9/12 overflow-y-auto pb-20">
-        {/* {videoLink && <ReactPlayer url={videoLink} width="100%" controls />} */}
-        {videoLink ? (
-          <VideoPlayer videoLink={videoLink} />
-        ) : (
-          <img src={course?.thumbnail} className="w-full rounded" />
-        )}
-
-        <div className="w-full shadow-lg shadow-stone-400 rounded-lg border py-4 mt-8">
-          <p className="text-xl font-bold px-3"> Quizzes</p>
-          <div className="flex flex-wrap">
-            <StartQuizContainer />
-            <StartQuizContainer />
-            <StartQuizContainer />
-          </div>
+    <div className="w-full grid md:flex p-4">
+      <div className=" md:grid md:grid-rows-6 md:grid-cols-4 md:gap-4 ">
+        <div className=" md:row-span-2 md:col-span-3 h-auto">
+            {/* {videoLink && <ReactPlayer url={videoLink} width="100%" controls />} */}
+          {videoLink ? (
+            <VideoPlayer videoLink={videoLink} />
+          ) : (
+            <img src={course?.thumbnail} className="w-full rounded" />
+          )}
         </div>
-      </div>
 
-      <div className="w-3/12 h-full  px-2 pb-16 ">
-        <div className="bg-slate-100 h-full shadow-lg shadow-stone-400 rounded-md overflow-y-auto">
+        <div className=" h-auto  px-2  md:row-span-8 md:col-span-1 ">
+        <div className="bg-slate-100 h-auto shadow-lg shadow-stone-400 rounded-md overflow-y-auto">
           {modules.map((module) => (
             <Module
               key={module._id}
@@ -50,6 +103,21 @@ export default function Course() {
           ))}
         </div>
       </div>
+        
+
+        <div className=" md:row-span-4 md:col-span-3 h-auto w-full shadow-lg shadow-stone-400 rounded-lg border pt-4 mt-8 mb-12 md:mb-0">
+          <p className="text-xl font-bold px-3"> Quizzes</p>
+          <div className="grid grid-cols-1 md:grid-cols-2  gap-1 mb-8">
+            <StartQuizContainer />
+            <StartQuizContainer />
+            <StartQuizContainer />
+          </div>
+        </div>
+
+        
+      </div>
+
+      
     </div>
   );
 }
