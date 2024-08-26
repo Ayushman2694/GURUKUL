99de1a46925d8c703d48d9ac020e4ab5e0efcac8
Ayushman2694 <sahuayushman4@gmail.com>
2024-08-26 13:44:02 +0530

    quiz update

--- backend/controllers/quiz.controller.js
+++ backend/controllers/quiz.controller.js
@@ -28,24 +28,29 @@ export const createQuiz = async (req, res) => {
 
 // Update a quiz
 export const updateQuiz = async (req, res) => {
-  const { id } = req.params;
-  const { title, questions } = req.body;
+  const { quizId, title, questions, moduleId } = req.body;
 
   try {
-    const quiz = await Quiz.findById(id);
+    const updateDetails = {
+      $set: {
+        title: title,
+        questions: questions,
+        moduleId: moduleId,
+      },
+    };
+
+    const quiz = await Quiz.findByIdAndUpdate(quizId, updateDetails, {
+      new: true,
+    });
 
     if (!quiz) {
-      return res.status(400).json({ message: "Quiz not found" });
+      return res.status(404).json({ error: "Quiz not found" });
     }
 
-    quiz.title = title;
-    quiz.questions = questions;
-
-    await quiz.save();
-
-    res.status(200).json(quiz);
+    return res.status(200).json({ message: "Quiz updated successfully", quiz });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.log(error.message)
+    return res.status(500).json({ error:"error in quiz update controller"});
   }
 };
 
