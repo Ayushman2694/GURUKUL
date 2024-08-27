06810a0a33465b59bb2d76826474039013b767b1
Ayushman2694 <sahuayushman4@gmail.com>
2024-08-27 12:58:23 +0530

    quiz attempt controller

--- backend/controllers/quiz.controller.js
+++ backend/controllers/quiz.controller.js
@@ -4,24 +4,20 @@ import QuizResponse from "../models/quizResponse.model.js";
 
 export const createQuiz = async (req, res) => {
   try {
-    const { title, moduleId, questions } = req.body;
-    const module = await Module.findById(moduleId);
-
-    if (!module) {
-      return res.status(400).json({ error: "Module not found" });
-    }
+    const { title, questions } = req.body;
 
     const quiz = new Quiz({
-      title,
-      module: moduleId,
-      questions,
+      title:title,
+      questions:questions,
+      attemptedBy: [],
     });
-
+    console.log(quiz)
     await quiz.save();
 
-    res.status(200).json({ message: "Quiz created successfully" });
+    return res.status(200).json({ message: "Quiz created successfully" });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.log(error.message);
+    return res.status(500).json({ message: error.message });
   }
 };
 
@@ -174,3 +170,52 @@ export const getResponseByResponseId = async (req, res) => {
       .json({ error: "Error in getResponseByResponseId controller" });
   }
 };
+
+export const quizByCourseId = async (req, res) => {
+  try {
+    const { courseId } = req.params;
+
+    const allModules = await Module.find({ course: courseId });
+
+    if (allModules.length === 0) {
+      return res.status(400).json({ error: "No modules found" });
+    }
+
+    const moduleIds = allModules.map((module) => module._id);
+
+    const allQuizzes = await Quiz.find({ module: { $in: moduleIds } });
+
+    return res.status(200).json({
+      message: "Successfully fetched all quizzes",
+      allQuizzes,
+    });
+  } catch (error) {
+    console.error(error.message);
+    return res
+      .status(500)
+      .json({ error: "Error in quizByCourseId Controller" });
+  }
+};
+export const quizAttempt=async(req,res)=>{
+  try {
+    const {empId,quizId,status}=req.body;
+    const quiz = await Quiz.findById(quizId);
+    if(!quiz){
+      return res.status(400).json({error:"unable to fetch quiz"})
+    }
+    else{
+    if(status==="pass"){
+      quiz.attemptedBy.push(empId)
+      quiz.passedBy.push(empId)
+    }
+    else{
+      quiz.attemptedBy.push(empId)
+  }
+    return res.status(200).json({message:"user attempted successfully"})}
+  } catch (error) {
+    console.error(error.message);
+    return res
+      .status(500)
+      .json({ error: "Error in quizAttempt Controller" });
+  }
+}
