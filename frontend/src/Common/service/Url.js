ee845235d8012f42fc566f1de96a532eea17c001
Ayushman2694 <sahuayushman4@gmail.com>
2024-10-03 13:00:19 +0530

    url update

--- frontend/src/Common/service/Url.js
+++ frontend/src/Common/service/Url.js
@@ -1,3 +1,3 @@
-const url = "http://localhost:6300";
-// const url = "https://gurukul-backend.onrender.com";
+// const url = "http://localhost:6300";
+const url =import.meta.env.BACKEND_URL ;
 export { url };
